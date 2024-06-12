8cd2950c025cb46061b3360da5e15f04c0ca2cc8
Nickie <minaev.36.ru@gmail.com>
2024-06-12 16:00:27 +0300

    add route to dashboard in views.py; add url to dashboard view in urls.py; add dashboard template

--- lottery/dashboard/views.py
+++ lottery/dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Good, Category
 
 # Create your views here.
+def dashboard(request):
+    goods = Good.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'goods': goods,
+        'categories': categories,
+    }
+    return render(request, 'dashboard_index.html', context)
