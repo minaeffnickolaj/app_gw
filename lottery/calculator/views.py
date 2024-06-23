0e84081f7a6963d1bc95d992882a9afb670577b6
Nickie <minaev.36.ru@gmail.com>
2024-06-23 06:56:41 +0300

    add calculator page  view calculator to views.py

--- lottery/calculator/views.py
+++ lottery/calculator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from dashboard.models import Good,Category
 # Create your views here.
+
+def calculator(request, context):
+    goods = Good.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'goods': goods,
+        'categories': categories,
+    }
+    return render(request, 'calculator_index.html', context)
