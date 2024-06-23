4bfe0c214721202d349d0f345bbd83644a3f355e
Nickie <minaev.36.ru@gmail.com>
2024-06-24 02:00:15 +0300

    write markup for calc, add basic functionality

---
+++ lottery/calculator/static/calculator/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  var rowIndex = 1;
+
+  $('.good-item').on('click', function(e) {
+    e.preventDefault();
+    
+    var name = $(this).data('name');
+    var cost = $(this).data('cost');
+    var pv = $(this).data('pv');
+
+    var newRow = `
+      <tr>
+        <td>${rowIndex}</td>
+        <td>${name}</td>
+        <td><input type="text" class="form-control cost-input" value="${cost}"></td>
+        <td><input type="checkbox"></td>
+        <td class="pv-value">${pv}</td>
+        <td><button class="btn btn-danger btn-sm delete-row">Удалить</button></td>
+      </tr>
+    `;
+
+    $('#totalsRow').before(newRow);
+    rowIndex++;
+    updateTotals();
+  });
+
+  $('#selectedItemsTable').on('click', '.delete-row', function() {
+    $(this).closest('tr').remove();
+    updateRowNumbers();
+    updateTotals();
+  });
+
+  $('#selectedItemsTable').on('input', '.cost-input', function() {
+    updateTotals();
+  });
+
+  //пересчет индексов строк
+  function updateRowNumbers() {
+    $('#selectedItemsTable tbody tr:not(#totalsRow)').each(function(index) {
+      $(this).find('td:first').text(index + 1);
+    });
+  }
+
+  //пересчет итогов
+  function updateTotals() {
+    var totalCost = 0;
+    var totalPV = 0;
+    
+    $('#selectedItemsTable tbody tr:not(#totalsRow)').each(function() {
+      var cost = parseFloat($(this).find('.cost-input').val()) || 0;
+      var pv = parseFloat($(this).find('.pv-value').text()) || 0;
+      totalCost += cost;
+      totalPV += pv;
+    });
+
+    $('#totalCost').text(totalCost.toFixed(2));
+    $('#totalPV').text(totalPV.toFixed(2));
+  }
+});
