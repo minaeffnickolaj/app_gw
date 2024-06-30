7b185bed39e9879797ae8e9fa372ed9474e42da4
Nickie Minaev <minaev.36.ru@gmail.com>
2024-06-30 10:43:04 +0300

    add additional config for mariadb

--- lottery/lottery/settings.py
+++ lottery/lottery/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306', cast=int),
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # кодировка соответствующая установленной в MariaDB
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # режим SQL для совместимости
+        },
     }
 }
 
